e69f1f86e2e2447b6ded9c486916387b10be37be
Guru Prasad <gurupras@buffalo.edu>
2014-04-14 10:40:23 -0400

    Added DictParser class and test cases

---
+++ src/com/games/wordladder/DictParser.java
@@ -0,0 +1,102 @@
+package com.games.wordladder;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class DictParser {
+	public static final String dictPath				= "libs/dictionary/";
+	private static Pattern dictionaryPattern		= Pattern.compile("(\\w+)\\s+\\(.*\\)\\s(.*)");
+	private static Map<String, String> dictionary	= new HashMap<String, String>(100000);
+
+	public static boolean init() throws DictionaryNotFound, DictionaryPatternException {
+		boolean succeeded = true;
+		StringBuffer errorBuffer = new StringBuffer();
+		File file = new File(dictPath);
+		if(!file.exists())
+			throw new DictionaryNotFound("Dictionary path " + dictPath + " does not exit");
+		
+		BufferedReader in = null;
+		try {
+			File[] files = file.listFiles();
+			for(File f : files) {
+				in = new BufferedReader(new FileReader(f));
+				String line;
+				while((line = in.readLine()) != null) {
+					if(line.equals(""))
+						continue;
+					Matcher m = dictionaryPattern.matcher(line);
+					if(!m.matches()) {
+						succeeded = false;
+						errorBuffer.append("Line did not match pattern\n" +
+								"File :" + f.getName() + "\n" +
+								"Line :" + line);
+						continue;
+					}
+					String word = m.group(1);
+					if(word.length() <= 3)
+						continue;
+					String defn = m.group(2);
+					dictionary.put(word, defn);
+				}
+				in.close();
+			}
+		} catch(IOException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			if(in != null) {
+				try { in.close(); } catch(IOException e) { System.err.println(e.getMessage()); }
+			}
+		}
+		if(errorBuffer.length() > 0) {
+			throw new DictionaryPatternException(errorBuffer.toString());
+		}
+		return succeeded;
+	}
+	
+	public static void storeDictionary(String filePath) throws FileNotFoundException, IOException {
+		File file = new File(filePath);
+		if(file.exists())
+			file.delete();
+		ObjectOutputStream out = null;
+		out = new ObjectOutputStream(new FileOutputStream(file));
+		out.writeObject(dictionary);
+		out.close();
+	}
+	
+	public static void loadDictionary(String filePath) throws FileNotFoundException, IOException, ClassNotFoundException {
+		File file = new File(filePath);
+		ObjectInputStream out = null;
+		out = new ObjectInputStream(new FileInputStream(file));
+		dictionary = (Map<String, String>) out.readObject();
+		out.close();
+	}
+	
+	
+	public static class DictionaryNotFound extends IOException {
+		public DictionaryNotFound(String string) {
+			super(string);
+		}
+	}
+	
+	public static class DictionaryPatternException extends Exception {
+		public DictionaryPatternException(String string) {
+			super(string);
+		}
+	}
+
+	public static Map<String, String> getDictionary() {
+		return dictionary;
+	}
+}
---
+++ src/test/dictionary/DictionaryTest.java
@@ -0,0 +1,129 @@
+package test.dictionary;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.games.wordladder.DictParser;
+import com.games.wordladder.DictParser.DictionaryPatternException;
+
+public class DictionaryTest {
+	private static StringBuffer result = null;
+	private static boolean verbose = false;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		result = new StringBuffer();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println(result.toString());
+	}
+
+	@Test
+	public void patternTest() {
+		StringBuffer localBuffer = new StringBuffer("Test whether dictionary pattern is correct              :");
+		StringBuffer errorBuffer = new StringBuffer();
+		boolean failed = false;
+		try {
+			DictParser.init();
+		} catch(DictionaryPatternException e) {
+			errorBuffer.append("Warning: Dictionary pattern failed to match all lines\n\t");
+			if(verbose)
+				errorBuffer.append(e.getMessage());
+		} catch(Exception e) {
+			failed = true;
+			errorBuffer.append(e.getMessage());
+			e.printStackTrace();
+		}
+		if(failed) {
+			localBuffer.append("Failed\n");
+		}
+		else
+			localBuffer.append("Passed\n");
+		localBuffer.append("\t" + errorBuffer + "\n");
+		result.append(localBuffer.toString());
+	}
+	
+	@Test
+	public void dictionarySerializationTest() {
+		StringBuffer localBuffer = new StringBuffer("Test whether dictionary serialization works             :");
+		StringBuffer errorBuffer = new StringBuffer();
+		boolean failed = false;
+		
+//		First check whether we're able to initialize the dictionary
+		try {
+			DictParser.init();
+		} catch(DictionaryPatternException e) {
+			errorBuffer.append("Warning: Dictionary pattern failed to match all lines\n\t");
+			if(verbose)
+				errorBuffer.append(e.getMessage());
+		} catch(Exception e) {
+			failed = true;
+			errorBuffer.append(e.getMessage());
+		}
+		if(failed) {
+			localBuffer.append("Failed\n");
+		}
+		
+//		Now do the actual SerDe
+		else {
+			Map<String, String> dictionary = DictParser.getDictionary();
+			try {
+				DictParser.storeDictionary("test.dict");
+			} catch(IOException e) {
+				failed = true;
+				errorBuffer.append(e.getMessage());
+			}
+			if(!failed) {
+				try {
+					DictParser.loadDictionary("test.dict");
+				} catch (Exception e) {
+					failed = true;
+					errorBuffer.append(e.getMessage());
+				}
+				if(!failed) {
+					Map<String, String> newDictionary = DictParser.getDictionary();
+					for(Map.Entry<String, String> entry : dictionary.entrySet()) {
+						String key = entry.getKey();
+						String value = entry.getValue();
+						if(!newDictionary.containsKey(key) || !newDictionary.get(key).equals(value)) {
+							failed = true;
+							errorBuffer.append("Serialized dictionary != deserialized dictionary");
+							break;
+						}
+					}
+					if(!failed) {
+						for(Map.Entry<String, String> entry : newDictionary.entrySet()) {
+							String key = entry.getKey();
+							String value = entry.getValue();
+							if(!dictionary.containsKey(key) || !dictionary.get(key).equals(value)) {
+								failed = true;
+								errorBuffer.append("Serialized dictionary != deserialized dictionary");
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		if(failed)
+			localBuffer.append("Failed\n");
+		else
+			localBuffer.append("Passed\n");
+		localBuffer.append("\t" + errorBuffer + "\n");
+		result.append(localBuffer.toString());
+		
+//		Clean up
+		File file = new File("test.dict");
+		if(file.exists())
+			file.delete();
+	}
+}
