5d3190bc265d0b967f26855e1ac3e9b65e58b471
Guru Prasad <gurupras@buffalo.edu>
2014-04-15 22:48:55 -0400

    Updated enum Difficulty with 'steps'.
    The field 'steps' corresponds to the number of steps that are necessary
    to exist between source and destination.

--- src/com/games/wordladder/Difficulty.java
+++ src/com/games/wordladder/Difficulty.java
@@ -1,8 +1,18 @@
 package com.games.wordladder;
 
 public enum Difficulty {
-	EASY,
-	MEDIUM,
-	HARD,
+	EASY (7),
+	MEDIUM (11),
+	HARD (15),
 	;
+	
+	private int steps;
+	
+	private Difficulty(int steps) {
+		this.steps = steps;
+	}
+	
+	public int getSteps() {
+		return steps;
+	}
 }
