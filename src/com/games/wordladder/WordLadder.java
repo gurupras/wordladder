72ab88b85076da9ad452c1c72aa350432990761d
Guru Prasad <gurupras@buffalo.edu>
2014-04-16 11:16:25 -0400

    Working on optimal recursive solution to WordLadder.getDestinations()

--- src/com/games/wordladder/WordLadder.java
+++ src/com/games/wordladder/WordLadder.java
@@ -21,58 +21,32 @@ public class WordLadder {
 		Map<String, Collection<String>> validDestinations = new HashMap<String, Collection<String>>();
 		while(validDestinations.size() < 1) {
 			this.origin			= dictionary.generateWord(maxWordLength);
-			getDestinations(origin, origin, steps, difficulty, validDestinations, null);
+			getDestinations(origin, origin, 0, difficulty.getMaxSteps(), difficulty, validDestinations, null);
 		}
 		int randomIndex		= (int) (0 + Math.random() * validDestinations.size());
 		this.destination	= (String) validDestinations.keySet().toArray()[randomIndex];
 		
 	}
 
-	private void getDestinations(String origin, String current, int steps, Difficulty difficulty, Map<String, Collection<String>> result, HashSet<String> stack) {
+	private void getDestinations(String origin, String current, int arrayIdx, int steps, Difficulty difficulty, Map<String, Collection<String>> result, Collection<String> stack) {
 		if(steps <= difficulty.getMinSteps()) {
 			assert(dictionary.containsKey(current));
 			result.put(current, stack);
 			if(steps == 0)
 				return;
 		}
-
-//		We have more steps to iterate..try all combinations possible
-		char[] array = current.toLowerCase().toCharArray();
-		for(int arrayIdx = 0; arrayIdx < array.length; arrayIdx++) {
-			char arrayChar = array[arrayIdx];
-			char permute = 'a';
+		char[] array = origin.toLowerCase().toCharArray();
+		for(; arrayIdx < array.length; arrayIdx++) {
+			char arrayChar	= array[arrayIdx];
+			char permute	= 'a';
 			while(permute <= 'z') {
 				if(permute != arrayChar) {
-//					We don't want to permute the same word over and over
 					array[arrayIdx] = permute;
-
 					String permutation = String.copyValueOf(array);
-					if(dictionary.containsKey(permutation)) {
-//						FIXME: Use a suffix tree to short-circuit this
-						if(stack == null) {
-//							Top-level of recursion
-							stack = new LinkedHashSet<String>();
-							stack.add(origin);
-						}
-						if(!stack.contains(permutation)) {
-							stack.add(permutation);
-							getDestinations(origin, permutation, steps - 1, difficulty, result, stack);
-						}
-					}
-					array[arrayIdx] = arrayChar;
+					getDestinations(origin, permutation, arrayIdx + 1, steps - 1, difficulty, result, new LinkedHashSet<String>());
 				}
-				permute++;
 			}
 		}
 	}
-	
-	private int hammingDistance(String origin, String destination) {
-		assert(origin.length() == destination.length()) : "Current implementation of hamming distance requires strings to be of equal length";
-		int result = 0;
-		for(int idx = 0; idx < origin.length(); idx++) {
-			if(origin.charAt(idx) != destination.charAt(idx))
-				result++;
-		}
-		return result;
-	}
 }
+	
