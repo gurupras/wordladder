2b946c6c8e03bbaedd853728016572e57633e642
Guru Prasad <gurupras@buffalo.edu>
2014-04-15 22:24:51 -0400

    Added empty class WordLadder

---
+++ src/com/games/wordladder/WordLadder.java
@@ -0,0 +1,10 @@
+package com.games.wordladder;
+
+public class WordLadder {
+	private Difficulty difficulty;
+	private String origin;
+	private String destination;
+	public WordLadder(Difficulty difficulty) {
+		
+	}
+}
