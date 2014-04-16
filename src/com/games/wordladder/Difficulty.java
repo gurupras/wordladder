bf5a088ba783c9537ecb307255ee397f75654887
Guru Prasad <gurupras@buffalo.edu>
2014-04-16 06:32:05 -0400

    Difficulty enum now stores     - maxSteps: Maximum optimal steps between two words     - minSteps: Minimum optimal steps between two words     - maxWordLength: Maximum permissible word length per difficulty

--- src/com/games/wordladder/Difficulty.java
+++ src/com/games/wordladder/Difficulty.java
@@ -1,18 +1,30 @@
 package com.games.wordladder;
 
 public enum Difficulty {
-	EASY (7),
-	MEDIUM (11),
-	HARD (15),
+	EASY (7, 3, 5),
+	MEDIUM (11, 6, 8),
+	HARD (15, 9, 12),
 	;
 	
-	private int steps;
+	private int maxSteps;
+	private int minSteps;
+	private int maxWordLength;
 	
-	private Difficulty(int steps) {
-		this.steps = steps;
+	private Difficulty(int maxSteps, int minSteps, int maxWordLength) {
+		this.maxSteps		= maxSteps;
+		this.minSteps		= minSteps;
+		this.maxWordLength	= maxWordLength;
 	}
 	
-	public int getSteps() {
-		return steps;
+	public int getMaxSteps() {
+		return maxSteps;
+	}
+	
+	public int getMinSteps() {
+		return minSteps;
+	}
+	
+	public int getMaxWordLength() {
+		return maxWordLength;
 	}
 }
